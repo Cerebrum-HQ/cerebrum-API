924f532e543d26c2a8e99966777b09f3bc6be03b
Patrick Njoli <patimjoli@gmail.com>
2021-01-18 10:52:37 +0100

    set up test file using jest and supertest

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/", (req, res, next) => {
-  res.send("<H1>Welcome to CEREBRUM</H1>");
+  res.send("Welcome to CEREBRUM");
   next();
 });
 
